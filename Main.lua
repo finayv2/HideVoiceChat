272195c720f4db5c744fd5e69d2c5759b45cf324
finayv2 <75693103+finayv2@users.noreply.github.com>
2022-07-18 19:34:52 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,4 +1,5 @@
 local Script = {}
+local suc, err;
 
 function Script:Init(Settings)
     local RealUserSettings = {
@@ -7,35 +8,48 @@ function Script:Init(Settings)
     }
 
     repeat task.wait() until game:IsLoaded()
-    if not game:GetService("VoiceChatService"):IsVoiceEnabledForUserIdAsync(game:GetService('Players').LocalPlayer.UserId) then return end
+    local VoiceChatEnabled;
+    suc, err = pcall(function()
+        VoiceChatEnabled = game:GetService("VoiceChatService"):IsVoiceEnabledForUserIdAsync(game:GetService('Players').LocalPlayer.UserId)
+    end)
+
+    if err or VoiceChatEnabled == nil then warn("Failed getting VoiceChatEnabled, Retrying..") Script:Init(Settings) return end
+    if not VoiceChatEnabled then return end
 
     local Notification = getrenv().require(game:GetService("CoreGui"):WaitForChild("RobloxGui"):WaitForChild("Modules"):WaitForChild("PromptCreator"))
+    local BubbleChatCore = game:GetService("CoreGui"):WaitForChild("BubbleChat")
     local BubbleChatString = "BubbleChat_"..tostring(game:GetService('Players').LocalPlayer.UserId)
+    local BubbleChatCombined = game:GetService("CoreGui"):WaitForChild("BubbleChat"):WaitForChild(BubbleChatString)
+    local voicebubble = BubbleChatCombined:WaitForChild("VoiceBubble")
     local ShowUIAgain = true;
-    game:GetService("CoreGui"):WaitForChild("BubbleChat"):WaitForChild(BubbleChatString):WaitForChild("VoiceBubble").Visible = false
 
-    game:GetService("CoreGui"):WaitForChild("BubbleChat"):WaitForChild(BubbleChatString).ChildAdded:Connect(function(Child)
+    voicebubble.Visible = false
+    BubbleChatCombined.ChildAdded:Connect(function(Child)
         if Child.Name == 'VoiceBubble' then
             Child.Visible = false
         end
     end)
 
-    function BeginCountDown()
-        task.spawn(function()
-            task.wait(20)
-            ShowUIAgain = true
-        end)
+    function PromptCallback(Option)
+        if Option then
+            ShowUIAgain = false
+            task.spawn(function()
+                task.wait(15)
+                ShowUIAgain = true
+            end)
+        else
+            ShowUIAgain = false
+        end
     end
 
     game:GetService("VoiceChatInternal").PlayerMicActivitySignalChange:Connect(function(Data)
-        if game:GetService("CoreGui"):WaitForChild("BubbleChat"):WaitForChild(BubbleChatString):WaitForChild("VoiceBubble").RoundedFrame.Contents.Insert.Image ~= 'rbxasset://textures/ui/VoiceChat/MicDark/Muted.png' then
-
+        if voicebubble.RoundedFrame.Contents.Insert.Image ~= 'rbxasset://textures/ui/VoiceChat/MicDark/Muted.png' then
             if Data.isActive then
                 if RealUserSettings['Show_Bubble_When_Speaking'] then
-                    game:GetService("CoreGui"):WaitForChild("BubbleChat"):WaitForChild(BubbleChatString):WaitForChild("VoiceBubble").Visible = true
+                    voicebubble.Visible = true
                 end
             else
-                game:GetService("CoreGui"):WaitForChild("BubbleChat"):WaitForChild(BubbleChatString):WaitForChild("VoiceBubble").Visible = false
+                voicebubble.Visible = false
             end
 
             if ShowUIAgain and RealUserSettings['Enable_Activity_Notifer'] then
@@ -46,14 +60,7 @@ function Script:Init(Settings)
                     CancelText = "Dont Ask Again",
                     CancelActive = true,
                     StripeColor = Color3.new(0.01, 0.72, 0.34),
-                    PromptCompletedCallback = function(Option)
-                        if Option then
-                            ShowUIAgain = false
-                            BeginCountDown()
-                        else
-                            ShowUIAgain = false
-                        end
-                    end,
+                    PromptCompletedCallback = PromptCallback(Option)
                 })
             end
         end
